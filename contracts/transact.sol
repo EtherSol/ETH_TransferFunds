83984830a5d6cbc040ea089b2c16bc1a490feb14
Kyle <kylemz@email.arizona.edu>
2022-06-08 17:01:18 -0700

    Second commit

---
+++ contracts/transact.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+contract transaction {
+    
+    address public minter;
+    mapping (address => uint) public balances;
+
+    event Sent(address from, address to, uint amount);
+
+    constructor() public {
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint amount) public {
+        require(msg.sender == minter);
+        balances[receiver] += amount;
+    }
+
+}
